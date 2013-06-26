f440e8f27a820ab07ae4c6cffb74b8ef56afa7d4
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 12:26:18 -0700

    added counter

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,25 +1,31 @@
 get '/' do
   # Look in app/views/index.erb
+  @new_url = Url.new
   erb :index
 end
 
 post '/url' do
-
-
   @long_url = params[:long_url]
-  new_url = Url.find_or_create_by_long_url(long_url:  @long_url)
-  @short_url =  'http://localhost:9393/' + new_url.short_url
-
-  erb :new_url
-
+  @new_url = Url.find_or_create_by_long_url(long_url:  @long_url)
+  if @new_url.valid?
+    @short_url =  'http://localhost:9393/' + @new_url.short_url
+    erb :new_url
+  else
+    erb :index
+  end
 end
 
 get '/:short_url' do
+
 
   p "Short_url: "
   p get_url = params[:short_url]
   p "Short url from DB: "
   new_url = Url.find_by_short_url(get_url)
   p new_url
+
+  new_url.counter += 1
+  p "Counter: #{new_url.counter}"
+  new_url.save
   redirect new_url.long_url
 end
--- db/migrate/20130625010101_create_table.rb
+++ db/migrate/20130625010101_create_table.rb
@@ -5,8 +5,9 @@ class CreateTable <ActiveRecord::Migration
 
   def change 
     create_table :urls do |t|
-      t.string :long_url
-      t.string :short_url
+      t.string    :long_url
+      t.string    :short_url
+      t.integer   :counter, :default => 0
     end
   end
 end
