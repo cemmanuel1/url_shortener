f2d9bea1a2aedc6934f356b371683f31172872be
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 11:37:33 -0700

    basic functionality working. need better validation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,24 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/url' do
+
+
+  @long_url = params[:long_url]
+  new_url = Url.find_or_create_by_long_url(long_url:  @long_url)
+  @short_url =  'http://localhost:9393/' + new_url.short_url
+
+  erb :new_url
+
+end
+
+get '/:short_url' do
+
+  p "Short_url: "
+  p get_url = params[:short_url]
+  p "Short url from DB: "
+  new_url = Url.find_by_short_url(get_url)
+  p new_url
+  redirect new_url.long_url
+end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,8 @@
 require 'uri'
 class Url < ActiveRecord::Base
   before_create :generate_short_url
-  validate :valid_url_or_ip
+  # validates :valid_url_or_ip
+  validates_format_of :long_url, :with => URI::regexp(%w(http https))
 
   def generate_short_url
     self.short_url = loop do
@@ -11,18 +12,18 @@ class Url < ActiveRecord::Base
   end
 
 
-  private
-  def valid_url_or_ip
-    unless valid_url?(long_url) || IPAddress.valid?(long_url)
-      errors.add(:long_url, "Not an URL or IP address")
-    end
-  end
+  # private
+  # def valid_url_or_ip
+  #   unless valid_url?(long_url)# || IPAddress.valid?(long_url)
+  #     errors.add(:long_url, "Not an URL or IP address")
+  #   end
+  # end
 
-  def valid_url?(long_url)
-    uri = URI.parse(long_url)
-    uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
-  rescue URI::InvalidURIError
-    false
-  end
+  # def valid_url?(long_url)
+  #   uri = URI.parse(long_url)
+  #   uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+  # rescue URI::InvalidURIError
+  #   false
+  # end
 
 end
